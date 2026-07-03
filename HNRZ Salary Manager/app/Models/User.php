80543e183285057d81867643a5bfebf2d7b791d0
MelNew <naneo1002@gmail.com>
2026-07-03 19:14:57 +0700

    fix admin verification
    Make email verification work when the user is not authenticated by: updating VerifyEmailController to accept the {id} and {hash} params, lookup the User, validate the email hash with hash_equals(sha1(...)), and mark the user verified while firing the Verified event. Swap the EmailVerificationRequest typehint for a generic Request and import the User model. Move the verification route out of the authenticated routes group (keeping signed and throttle middleware) so the signed URL can be used without an active session. Also add the HasRoles trait to the User model. These changes keep the signed URL and throttle protections while enabling verification links to be clicked by users who are logged out.

--- HNRZ Salary Manager/app/Models/User.php
+++ HNRZ Salary Manager/app/Models/User.php
@@ -9,14 +9,15 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Traits\HasRoles; 
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * Get the attributes that should be cast.
