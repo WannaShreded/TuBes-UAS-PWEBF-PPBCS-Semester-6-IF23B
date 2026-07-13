2d3b732ec270770b6d51de7e5c2976a48c499ebb
MelNew <naneo1002@gmail.com>
2026-07-13 16:49:06 +0700

    add logo
    Replace generic Laravel welcome page with branded PayrollKu landing page featuring cosmic design, feature cards, and call-to-action buttons. Add HNRZ logo assets, update favicon, set database password configuration, and change API endpoint to localhost.

--- hnrz_salary_manager/lib/services/api_client.dart
+++ hnrz_salary_manager/lib/services/api_client.dart
@@ -1,4 +1,4 @@
 class ApiClient {
   // static const String baseUrl = "http://192.168.100.14:8000/api";
-  static const String baseUrl = "http://10.0.2.2:8000/api";
+  static const String baseUrl = "http://127.0.0.1:8000/api";
 }
