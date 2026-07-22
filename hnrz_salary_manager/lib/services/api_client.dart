075acfdb70001e9819e196d616588b3119700c2d
IkiRahman <ikirahman152@gmail.com>
2026-07-22 15:45:43 +0700

    build apk

--- hnrz_salary_manager/lib/services/api_client.dart
+++ hnrz_salary_manager/lib/services/api_client.dart
@@ -3,5 +3,5 @@ class ApiClient {
   // static const String baseUrl = "http://172.16.11.188:8000/api";
   // static const String baseUrl = "http://172.16.11.188:8000/api";
   // static const String baseUrl = "http://172.16.11.188:8000/api";
-  static const String baseUrl = "http://127.0.0.1:8000/api";
+  static const String baseUrl = "http://192.168.1.201:8000/api";
 }
